f63230d7a13f79b43ba142bd9852e3c84e2ab678
MarcinDrossel <marcindrossel04@gmail.com>
2022-03-22 17:16:25 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Image, View, Text } from 'react-native';
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Cześć! {this.props.name}</Text>
+    );
+  }
+}
+
+export default class HelloWorldApp extends Component {
+  render(){
+  return (
+    <View style={{alignItems: 'center'}}>
+      <Greeting name='Ala' />
+      <Greeting name='Marcin' />
+      <Greeting name='Filip' />
+      <Greeting name='Asia' />
+      <Greeting name='Olek' />
+    </View>
+  );
+ };
+}
