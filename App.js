c0dcddce2a312707c137c3441a9cfa57b2199065
MarcinDrossel <marcindrossel04@gmail.com>
2022-03-23 08:37:58 +0100

    Wymiary Fix

--- App.js
+++ App.js
@@ -1,26 +1,14 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View, } from 'react-native';
 
-export default class LotsOfStyles extends Component {
+export default class FixedDimensionBasic extends Component {
   render() {
     return (
       <View>
-        <Text style={Styles.red}>To jest czerwone</Text>
-        <Text style={Styles.bigblue}>To jest pogrubione i niebieskie</Text>
-        <Text style={[Styles.bigblue, Styles.red]}>To jest niebieski z czerownym</Text>
-        <Text style={[Styles.red, Styles.bigblue]}>To jest czerwony z niebieskim</Text>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
       </View>
-    );
+    )
   }
 }
-
-const Styles = StyleSheet.create({
-  bigblue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-});
