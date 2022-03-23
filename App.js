4c06557f447e26e71c396682ddc2bce2b9e4c90f
MarcinDrossel <marcindrossel04@gmail.com>
2022-03-23 10:09:08 +0100

    TextInput zmiana słów

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, TextInput, } from 'react-native';
 
-export default class FlexDimensionBasic extends Component {
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 2, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 3, height: 150, backgroundColor: 'steelblue'}} />
-      </View>
-    )
+export default class Translation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
+render () {
+  return(
+    <View style={{padding: 10}}>
+      <TextInput 
+      xstyle={{height: 40}}
+      placeholder='Wpisz cokolwiek...'
+      onChangeText={(text) => this.setState({text})} 
+      />
+      <Text style={{padding: 40, fontSize: 42}}>
+        {this.state.text.split(' ').map(word => word && 'X').join(' ')}
+      </Text>
+    </View>
+    );
   }
 }
