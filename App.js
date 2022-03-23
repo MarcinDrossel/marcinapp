6c1b8db5067357bee7d721919415bca3e06770b9
MarcinDrossel <marcindrossel04@gmail.com>
2022-03-23 08:42:50 +0100

    Wymiary 2 Flex

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View, } from 'react-native';
 
-export default class FixedDimensionBasic extends Component {
+export default class FlexDimensionBasic extends Component {
   render() {
     return (
-      <View>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, height: 150, backgroundColor: 'steelblue'}} />
       </View>
     )
   }
