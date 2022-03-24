ecf16341e9df55a3a8f6ba78072b4082285e41c0
MarcinDrossel <marcindrossel04@gmail.com>
2022-03-24 11:28:55 +0100

    Api, pobieranie danych

--- App.js
+++ App.js
@@ -1,39 +1,48 @@
 import React, { Component } from 'react';
 import {
   FlatList,
-  StyleSheet,
+  ActivityIndicator,
   View,
   Text,
 } from 'react-native';
 
-export default class FlatListBasic extends Component {
+export default class FetchExample extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isLoading: true};
+  }
+  
+  async componentDidMount() {
+    try {
+      const response = await fetch ('https://facebook.github.io/react-native/movies.json');
+      const responseJson = await response.json();
+
+      this.setState({
+        isLoading: false,
+        dataSource: responseJson.movies,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
     return(
-      <View style={styles.container}>
+      <View style={{flex: 1, paddingTop: 20}}>
         <FlatList
-        data={[
-          {key: 'Devin'},
-          {key: 'Jackson'},
-          {key: 'Joel'},
-          {key: 'John'},
-          {key: 'Jilliam'},
-          {key: 'Julie'},
-        ]}
-        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-        />
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
+          keyExtractor={(item, index) => index}
+          />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 22,
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-});
