8f891019d623d45971beba3134d6770b3662d27f
MarcinDrossel <marcindrossel04@gmail.com>
2022-03-24 08:47:28 +0100

    ScrollView góra, dół

--- App.js
+++ App.js
@@ -1,77 +1,45 @@
 import React, { Component } from 'react';
 import {
-  Alert,
-  Platform,
-  StyleSheet,
-  Text, 
-  TouchableHighlight,
-  TouchableNativeFeedback,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-  View,
+  ScrollView,
+  Image,
+  Text,
 } from 'react-native';
 
-export default class Touchables extends Component {
-  onPressButton() {
-    Alert.alert('Nacisnąłeś przycisk!');
-  }
-
-  onLongPressButton() {
-    Alert.alert('Długo naciskasz przycisk!');
-  }
-
+export default class ScrollingDown extends Component {
   render() {
-    return(
-      <View style={styles.container}>
-        <TouchableHighlight onPress={this.onPressButton} underlayColor='white'>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableHighlight</Text>
-          </View>
-        </TouchableHighlight>
-
-        <TouchableOpacity onPress={this.onPressButton}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableOpacity</Text>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableNativeFeedback onPress={this.onPressButton}
-        background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
-          </View>
-        </TouchableNativeFeedback>
-
-        <TouchableWithoutFeedback onPress={this.onPressButton}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
-          </View>
-        </TouchableWithoutFeedback>
-
-        <TouchableHighlight onLongPress={this.onLongPressButton} onPress={this.onPressButton} underlayColor='white'>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableHighlight</Text>
-          </View>
-        </TouchableHighlight>
-      </View>
+    return (
+      <ScrollView>
+        <Text style={{fontSize: 96}}>Scroll me plz</Text>
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Text style={{fontSize: 96}}>Scroll me plz</Text>
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Text style={{fontSize: 96}}>Scroll me plz</Text>
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Text style={{fontSize: 96}}>Scroll me plz</Text>
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Text style={{fontSize: 96}}>Scroll me plz</Text>
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+        <Image style={{width: 200, height: 200,}} source={{uri: 'https://cdn-images-1.medium.com/max/796/1*jh6bmapyE8nPWju7W_7qEw.png'}} />
+      </ScrollView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 60,
-    alignItems: 'center',
-  },
-  button: {
-    marginBottom: 40,
-    width: 200,
-    alignItems: 'center',
-    backgroundColor: '#2196F3',
-  },
-  buttonText: {
-    padding: 20,
-    color: 'white',
-  },
-});
-
